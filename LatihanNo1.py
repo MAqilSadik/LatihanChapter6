388fb80067fe61035a95aa6228555aa7f8b825c2
MAqilSadik <aqilsadik@users.noreply.github.com>
2020-11-05 22:56:23 +0700

    Create LatihanNo1.py

---
+++ LatihanNo1.py
@@ -0,0 +1,15 @@
+def isPythagoras(a,b,c):
+    if(a**2 + b**2 == c**2):
+        print(True)
+    
+    else:
+        print(False)
+
+
+print(1),isPythagoras(3,4,5)
+
+print(2),isPythagoras(5,9,12)
+
+print(3),isPythagoras(8,6,10)
+
+print(4),isPythagoras(7,8,11)
